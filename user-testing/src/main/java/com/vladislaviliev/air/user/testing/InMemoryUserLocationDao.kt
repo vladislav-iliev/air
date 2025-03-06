1da2fb140f7efd638ce58e14442be5414e683fcf
Vladislav Iliev <vladislav.iliev@hotmail.com>
2025-03-06 02:36:01 +0200

    For InMemoryUserLocationDao, guard both [locations, lastId] with a mutex, enforcing invariant at all times

--- user-testing/src/main/java/com/vladislaviliev/air/user/testing/InMemoryUserLocationDao.kt
+++ user-testing/src/main/java/com/vladislaviliev/air/user/testing/InMemoryUserLocationDao.kt
@@ -1,38 +1,53 @@
 package com.vladislaviliev.air.user.testing
 
+import androidx.annotation.GuardedBy
+import androidx.paging.PagingSource
 import com.vladislaviliev.air.user.location.Dao
+import com.vladislaviliev.air.user.location.LocationNotFoundException
 import com.vladislaviliev.air.user.location.UserLocation
-import java.util.concurrent.atomic.AtomicInteger
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import kotlin.math.max
 
 class InMemoryUserLocationDao : Dao {
 
-    private var lastId = AtomicInteger(0)
+    private val mutex = Mutex()
+
+    @GuardedBy("mutex")
     private val locations = mutableMapOf<Int, UserLocation>()
 
-    override suspend fun get(id: Int) = locations[id] ?: throw NoSuchElementException()
+    @GuardedBy("mutex")
+    private var lastId = 0
 
-    override suspend fun upsert(userLocation: UserLocation) {
-        val newId = if (userLocation.id == 0) lastId.incrementAndGet() else userLocation.id
+    override suspend fun get(id: Int) =
+        mutex.withLock { locations[id] } ?: throw LocationNotFoundException()
+
+    override suspend fun upsert(userLocation: UserLocation) = mutex.withLock {
+        val newId = if (userLocation.id == 0) lastId + 1 else userLocation.id
+        lastId = max(lastId, newId)
         locations[newId] = userLocation
     }
 
-    override suspend fun upsert(userLocations: Collection<UserLocation>) {
+    override suspend fun upsert(userLocations: Collection<UserLocation>) =
         userLocations.forEach { upsert(it) }
+
+    override suspend fun exists(name: String) =
+        mutex.withLock { locations.values }.any { it.name == name }
+
+    override suspend fun getLastId() = mutex.withLock { lastId }
+
+    override suspend fun delete(ids: Collection<Int>) =
+        ids.forEach { mutex.withLock { locations.remove(it) } }
+
+    override suspend fun deleteAllExcept(id: Int) = mutex.withLock { locations.keys }
+        .filter { it != id }
+        .forEach { mutex.withLock { locations.remove(it) } }
+
+    override fun newPagingSource(excluding: Int): PagingSource<Int, UserLocation> {
+        val locations = runBlocking { mutex.withLock { locations.values } }
+            .filter { it.id != excluding }
+            .sortedBy { it.name.first().uppercaseChar() }
+        return StaticListPagingSource(locations)
     }
-
-    override suspend fun exists(name: String) = locations.values.any { it.name == name }
-
-    override suspend fun getLastId() = lastId.get()
-
-    override suspend fun delete(ids: Collection<Int>) {
-        ids.forEach { locations.remove(it) }
-    }
-
-    override suspend fun deleteAllExcept(id: Int) {
-        locations.keys.filter { it != id }.forEach { locations.remove(it) }
-    }
-
-    override fun newPagingSource(excluding: Int) = StaticListPagingSource(
-        locations.values.filter { it.id != excluding }.sortedBy { it.name.first().uppercaseChar() }
-    )
 }
