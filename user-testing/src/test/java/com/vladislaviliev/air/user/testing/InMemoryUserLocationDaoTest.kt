b3bff98414aa4bbf1c2489b9915077d2c55c8f1a
Vladislav Iliev <vladislav.iliev@hotmail.com>
2025-03-11 22:49:55 +0200

    In InMemoryUserLocationDaoTest, set test dispatcher to default test dispatcher

--- user-testing/src/test/java/com/vladislaviliev/air/user/testing/InMemoryUserLocationDaoTest.kt
+++ user-testing/src/test/java/com/vladislaviliev/air/user/testing/InMemoryUserLocationDaoTest.kt
@@ -1,26 +1,26 @@
 package com.vladislaviliev.air.user.testing
 
 import com.vladislaviliev.air.user.location.UserLocation
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert
 import org.junit.Test
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class InMemoryUserLocationDaoTest {
 
     @Test
-    fun upsert_is_synchronized() = runBlocking(Dispatchers.Default) {
+    fun upsert_is_synchronized() = runTest {
         val dao = InMemoryUserLocationDao()
         val n = 50_000
 
-        coroutineScope {
-            repeat(n) {
-                launch { dao.upsert(UserLocation(0, "", 0.0, 0.0)) }
-            }
+        repeat(n) {
+            launch { dao.upsert(UserLocation(0, "", 0.0, 0.0)) }
         }
 
+        advanceUntilIdle()
         Assert.assertEquals(n, dao.getLastId())
     }
 }
